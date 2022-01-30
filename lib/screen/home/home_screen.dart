169e5410cc7f689a61a757090da0d7dbce819dfd
Mohammadshahsamirbhu <samir_reaz@yahoo.com>
2022-01-30 13:44:06 +0600

    home page designe

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -7,6 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white.withOpacity(.8),
       appBar: AppBar(
         backgroundColor: Colors.blue,
         leading: Icon(
@@ -51,6 +52,12 @@ class HomeScreen extends StatelessWidget {
                   },
                   child: Container(
                     decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            'https://previews.123rf.com/images/suksao/suksao1704/suksao170400002/75460320-medicine-is-pill-and-capsule-clock-show-12-am-or-pm-with-copy-space-on-wood-table-.jpg',
+                          ),
+                        ),
                         color: Colors.amber,
                         borderRadius: BorderRadius.circular(10)),
                     height: 200,
@@ -66,7 +73,15 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   Expanded(
                     child: Container(
-                      color: Colors.pink,
+                      decoration: BoxDecoration(
+                        color: Colors.pink,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            'https://cdn2.momjunction.com/wp-content/uploads/2020/11/17-Simple-And-Useful-Health-Tips-For-Children-To-Follow-Banner-910x1024.jpg',
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                   SizedBox(width: 10),
@@ -76,19 +91,30 @@ class HomeScreen extends StatelessWidget {
                         Expanded(
                           flex: 5,
                           child: Container(
-                            color: Colors.green,
-                            height: 150,
-                            width: 180,
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                  'https://brettpodolsky.com/wp-content/uploads/2019/02/Depositphotos_103806958_m-2015.jpg',
+                                ),
+                              ),
+                            ),
                           ),
                         ),
                         SizedBox(height: 10),
                         Expanded(
                           flex: 3,
                           child: Container(
-                            color: Colors.blue,
-                            height: 50,
-                            width: 180,
-                          ),
+                              decoration: BoxDecoration(
+                            color: Colors.green,
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                'https://www.cdc.gov/fungal/diseases/aspergillosis/images/318301-A_WEB_FungalLandingPages_Candidiasis_Symptoms.jpg',
+                              ),
+                            ),
+                          )),
                         ),
                       ],
                     ),
