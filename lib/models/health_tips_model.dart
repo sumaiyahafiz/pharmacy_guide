771019cbcceb78624f35c8008306938be61d3d06
HP <sumaiyahafiz993@gamil.com>
2022-02-03 12:15:20 +0600

    Modetips chanege

--- lib/models/health_tips_model.dart
+++ lib/models/health_tips_model.dart
@@ -1,1 +1,38 @@
+class Healthtips_Model {
+  final String Name, image;
 
+  Healthtips_Model({required this.Name, required this.image});
+}
+
+List<Healthtips_Model> H_Tips = [
+  Healthtips_Model(
+    Name: 'Food & Nutrition',
+    image:
+        'https://cdn.simplesite.com/i/92/08/282319409929586834/i282319414677948144._szw1280h1280_.jpg',
+  ),
+  Healthtips_Model(
+    Name: 'Food & Nutrition',
+    image:
+        'https://betterme.world/articles/wp-content/uploads/2020/09/shutterstock_1667782504.jpg',
+  ),
+  Healthtips_Model(
+    Name: 'Food & Nutrition',
+    image:
+        'https://betterme.world/articles/wp-content/uploads/2020/09/shutterstock_1667782504.jpg',
+  ),
+  Healthtips_Model(
+    Name: 'Food & Nutrition',
+    image:
+        'https://betterme.world/articles/wp-content/uploads/2020/09/shutterstock_1667782504.jpg',
+  ),
+  Healthtips_Model(
+    Name: 'Food & Nutrition',
+    image:
+        'https://betterme.world/articles/wp-content/uploads/2020/09/shutterstock_1667782504.jpg',
+  ),
+  Healthtips_Model(
+    Name: 'Food & Nutrition',
+    image:
+        'https://betterme.world/articles/wp-content/uploads/2020/09/shutterstock_1667782504.jpg',
+  ),
+];
